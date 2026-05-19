86f4c7319346122ae042dfc63fce364778b556b5
jonardnava092600 <164429202+jonardnava092600@users.noreply.github.com>
2026-05-20 00:04:07 +0800

    Add files via upload

---
+++ my-portfolio-web/components/main/home.php
@@ -0,0 +1,111 @@
+<div class="d-flex flex-row justify-content-center" id="profileWrapper">
+    <img id="profilePicture" src="./assets/img/porfile-picture.png" alt="propayl">
+    <div>
+        <p id="homeName" class="mb-0 name-display-class">Jonard Beradio Nava</p>
+        <p id="homeRole" class="m-0 role-display-class">Programmer</p>
+        
+        <p id="homeTools" class="mt-3 mb-0">Tools:</p>
+        <div id="homeToolWrapper" class="tools-wrapper">
+            <div class="tools-track d-flex flex-row ">
+                <a href="https://dart.dev/">
+                    <i class="devicon-dart-plain colored"></i>
+                    <span>Dart</span>
+                </a>
+                <a href="https://www.php.net/">
+                    <i class="devicon-php-plain colored"></i>
+                    <span>PHP</span> 
+                </a>
+                <a href="https://www.w3schools.com/js/">
+                    <i class="devicon-javascript-plain colored"></i>
+                    <span>JavaScript</span>
+                </a>
+                <a href="https://www.python.org/">
+                    <i class="devicon-python-plain colored"></i>
+                    <span>Python</span>
+                </a>
+                <a href="https://www.w3schools.com/Html/">
+                    <i class="devicon-html5-plain colored"></i>
+                    <span>HTML</span>
+                </a>
+                <a href="https://www.w3schools.com/css/">
+                    <i class="devicon-css3-plain colored"></i>
+                    <span>CSS</span>
+                </a>
+                <a href="https://www.mysql.com/ ">
+                    <i class="devicon-mysql-plain colored"></i>
+                    <span>MySQL</span>
+                </a>
+                <a href="https://flutter.dev/">
+                    <i class="devicon-flutter-plain colored"></i>
+                    <span>Flutter</span>
+                </a>
+                
+
+                <a href="https://dart.dev/">
+                    <i class="devicon-dart-plain colored"></i>
+                    <span>Dart</span>
+                </a>
+                <a href="https://www.php.net/">
+                    <i class="devicon-php-plain colored"></i>
+                    <span>PHP</span> 
+                </a>
+                <a href="https://www.w3schools.com/js/">
+                    <i class="devicon-javascript-plain colored"></i>
+                    <span>JavaScript</span>
+                </a>
+                <a href="https://www.python.org/">
+                    <i class="devicon-python-plain colored"></i>
+                    <span>Python</span>
+                </a>
+                <a href="https://www.w3schools.com/Html/">
+                    <i class="devicon-html5-plain colored"></i>
+                    <span>HTML</span>
+                </a>
+                <a href="https://www.w3schools.com/css/">
+                    <i class="devicon-css3-plain colored"></i>
+                    <span>CSS</span>
+                </a>
+                <a href="https://www.mysql.com/ ">
+                    <i class="devicon-mysql-plain colored"></i>
+                    <span>MySQL</span>
+                </a>
+                <a href="https://flutter.dev/">
+                    <i class="devicon-flutter-plain colored"></i>
+                    <span>Flutter</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="socialsSection">
+            <a class="anchor-style" href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSMVkjMfRnQrqggpVqCdCcFqjFfTSgBCmZHQPwLJhLdCPlXkqCHzqpCnxzcLSCzchdqCDKTm">
+                <div>
+                    <i class="fas fa-envelope"></i>
+                    <span>jonardnava2@gmail.com</span>
+                </div>
+            </a>
+            <a class="anchor-style" href="https://www.linkedin.com/in/jonard-beradio-nava-4464a7371/">
+                <div>
+                    <i class="fab fa-linkedin"></i>
+                    <span class="d-flex flex-row">Jonard&nbspBeradio&nbspNava</span>
+                </div>
+            </a>
+            <a class="anchor-style" href="https://www.facebook.com/n4dzkie">
+                <div>
+                    <i class="fab fa-facebook"></i>
+                    <span class="d-flex flex-row">Jonard&nbspNava</span>
+                </div>
+            </a>
+            <a class="anchor-style" href="https://www.instagram.com/nadxyzqwerty/">
+                <div>
+                    <i class="fab fa-instagram"></i>
+                    <span>nadxyzqwerty</span>
+                </div>
+            </a>
+            <a class="anchor-style" href="https://www.tiktok.com/@jnrdnvx">
+                <div>
+                    <i class="fab fa-tiktok"></i>
+                    <span>nadxyz</span>
+                </div>
+            </a>
+        </div>
